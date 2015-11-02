5d5d7e1094ec5fc1b849573c9a020538051c112b
cglangan78 <cglangan78@gmail.com>
2015-11-02 14:31:36 -0800

    added controllers and mock users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/show'
+
+  get 'pictures/index'
+
+  get 'pictures/show'
+
+  get 'users/index'
+
+  get 'users/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
