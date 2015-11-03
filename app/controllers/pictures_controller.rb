189222dd83fbf20baa9eedabeaaab06630c99209
cglangan78 <cglangan78@gmail.com>
2015-11-02 19:27:50 -0800

    connected picture to user

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -14,11 +14,12 @@ class PicturesController < ApplicationController
 
   def new
     @picture = Picture.new
+    @user = current_user
   end
 
   def create
     @picture = Picture.new(picture_params)
-
+    @picture.user_id = current_user.id
     if @picture.save
       redirect_to('/')
     else
